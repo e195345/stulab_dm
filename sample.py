6d15156c7d9b08ea14b40a460d50c7e400072ae2
Soya Uehara <e195752@ie.u-ryukyu.ac.jp>
2021-07-01 15:58:18 +0900

    Code review commit

--- sample.py
+++ sample.py
@@ -1,46 +1,12 @@
 import pandas as pd
-import math
 from sklearn import metrics, preprocessing
 from sklearn.svm import LinearSVC
 import numpy as np
-import collections
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 # データ読み込み
-temperature_df = pd.read_csv("temperature.csv", skiprows=5, header=None, names=['年月日時', '気温', '気温_品質'])
-rain_df = pd.read_csv("rain.csv", skiprows=5, header=None, names=['年月日時', '降水量', '降水量_品質', '降水量_情報'])
-weather_df = pd.read_csv("weather.csv", skiprows=5, header=None, names=['年月日時', 'label', '天気_品質'])
-cloud_df = pd.read_csv("cloud.csv", skiprows=5, header=None, names=['年月日時', '雲量', '雲量_品質'])
+df = pd.read_csv("./1year.csv", skiprows=1).dropna(how='any').reset_index(drop=True)
 
-df_t_r = pd.merge(temperature_df, rain_df, on='年月日時')
-df_t_r_c = pd.merge(df_t_r, cloud_df, on='年月日時')
-df = pd.merge(df_t_r_c, weather_df, on='年月日時')
-
-weather_num = 1.0
-cloud_num = 9.5
-
-# nanの処理
-'''
-for i in range(len(df)):
-    if math.isnan(df.at[i, 'label']):
-        df.at[i, 'label'] = weather_num
-    else:
-        weather_num = df.at[i, 'label']
-
-for i in range(len(df)):
-    if math.isnan(df.at[i, '雲量']):
-        df.at[i, '雲量'] = cloud_num
-    else:
-        cloud_num = df.at[i, '雲量']
-'''
-
-rain_num = 2.0
-for i in range(len(df)):
-    if (df.at[i, '降水量'] < rain_num) & (df.at[i, 'label'] == 10.0):
-        df.at[i, '降水量'] = rain_num
-
-# 気温の差分で学習
+# 前処理
 previous_day_temperature = 0.0
 for i in range(len(df)):
     if i == 0:
@@ -51,8 +17,7 @@ for i in range(len(df)):
         previous_day_temperature = df.at[i, '気温']
         df.at[i, '気温'] = temperature_difference
 
-df = df[['気温', '降水量', '雲量', 'label']]
-df = df.dropna(how='any')
+df = df.dropna(how='any').reset_index(drop=True)
 
 X = df[['気温', '降水量', '雲量']]
 Y = df['label']
@@ -62,10 +27,10 @@ sc.fit(X)
 X_normal = pd.DataFrame(sc.transform(X))
 X_normal.columns = ['気温', '降水量', '雲量']
 
-#
-half = int(len(df)*0.8)
-X_train, X_test = X_normal[:half], X_normal[half:]
-Y_train, Y_test = Y[:half], Y[half:]
+# トレーニングデータとテストデータに分ける
+partition = int(len(df)*0.8)
+X_train, X_test = X_normal[:partition], X_normal[partition:]
+Y_train, Y_test = Y[:partition], Y[partition:]
 
 clf_result = LinearSVC(max_iter=1000000, C=5.0)
 clf_result.fit(X_train, Y_train)
@@ -79,6 +44,3 @@ print('正答率：{:.3f}%'.format(ac_score*100))
 Mixed_matrix = metrics.confusion_matrix(Y_test, pre)
 print("標準化")
 print(Mixed_matrix)
-
-
-
